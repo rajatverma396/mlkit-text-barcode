8e3230201cd21cb2e254236721bcc031f626723e
Rajat verma <rajat.verma396@gmail.com>
2021-07-21 14:10:19 +0530

    initial commit

---
+++ android/src/main/java/com/google_ml_kit/MlKitMethodCallHandler.java
@@ -0,0 +1,48 @@
+package com.google_ml_kit;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.google_ml_kit.vision.BarcodeDetector;
+import com.google_ml_kit.vision.TextDetector;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+
+public class MlKitMethodCallHandler implements MethodChannel.MethodCallHandler {
+
+    private final Map<String, ApiDetectorInterface> handlers;
+
+    public MlKitMethodCallHandler(Context context) {
+        List<ApiDetectorInterface> detectors = new ArrayList<ApiDetectorInterface>(
+                Arrays.asList(
+                        new BarcodeDetector(context),
+                        new TextDetector(context)
+
+                ));
+
+        handlers = new HashMap<>();
+        for (ApiDetectorInterface detector : detectors) {
+            for (String method : detector.getMethodsKeys()) {
+                handlers.put(method, detector);
+            }
+        }
+    }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
+        ApiDetectorInterface handler = handlers.get(call.method);
+        if (handler != null) {
+            handler.onMethodCall(call, result);
+        } else {
+            result.notImplemented();
+        }
+    }
+}
